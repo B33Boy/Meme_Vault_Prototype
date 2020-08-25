ec016705b3e4957be15f964b3b22e504407a0d55
B33Boy <abhip202@gmail.com>
2020-08-24 20:13:56 -0400

    fix: redirect to login

--- application/main/main.py
+++ application/main/main.py
@@ -10,6 +10,13 @@ main_bp = Blueprint('main_bp', __name__,
                     template_folder='templates', static_folder='static', static_url_path='')
 
 
+
+@main_bp.route('/')
+def home():
+    
+    return redirect(url_for('login_bp.login'))
+
+
 @main_bp.route('/<username>')
 @login_required
 def index(username):
